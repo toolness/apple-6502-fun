8570dc40543c491d921aba2db081daebe8510223
Atul Varma <varmaa@gmail.com>
2023-11-25 15:20:01 -0500

    Add al05-paddle-1a.

---
+++ src/al05-paddle-1a.s
@@ -0,0 +1,21 @@
+* AL05-PADDLE PROGRAM 1 from Assembly Lines, pg. 32
+
+* Note that running this on Virtual ][ requries paddle config.
+* Easiest way to do this is to use the pull-down menu option
+* "Quick settings -> Use Arrows as Joystick".
+* This isn't ideal because it only sends 3 discrete values,
+* but it's better than nothing.
+
+	ORG $300
+PREAD	EQU $FB1E
+HOME	EQU $FC58
+COUT	EQU $FDED
+
+START	JSR HOME
+	LDX #$00	; Can change to 1 and use up/down arrows!
+LOOP	JSR PREAD
+	TYA
+	JSR COUT
+	JMP LOOP
+
+* INFINITE LOOP (Press "Reset" in Virtaul ][ to abort)
